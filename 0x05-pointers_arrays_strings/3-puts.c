a925929679ca5f13ff95ee50f405c4bee54067e1
Namountougou Dimanche <dimanchenamo@gmail.com>
2022-10-24 09:29:03 -0700

    implement task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - print a string and folowed by a new line
+ * @str : string
+ */
+
+void _puts(char *str)
+{
+	while (*str != '0\')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
