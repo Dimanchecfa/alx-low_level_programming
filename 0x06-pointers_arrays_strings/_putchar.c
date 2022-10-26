3e1f61e20c82581849a7dd4d2a83af77526d43d6
Namountougou Dimanche <dimanchenamo@gmail.com>
2022-10-26 09:06:29 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+*_putchar - writes the caracter c to stdout
+* @c: the caracter to print
+*
+*Return: On succes 1
+*/
+
+int _putchar(char c)
+{
+	return (write(1 , &c , 1));
+}
